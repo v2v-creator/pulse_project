20a28fa7e4dc8e66be9ef9048e9148438834362f
Mihail <mivalb79@gmail.com>
2020-01-23 23:37:56 +0300

    add function toggleSlide

--- src/js/script.js
+++ src/js/script.js
@@ -28,4 +28,21 @@ $(document).ready(function() {
       .eq($(this).index())
       .addClass("catalog__content_active");
   });
+
+  function toggleSlide(item) {
+    $(item).each(function(i) {
+      $(this).on("click", function(e) {
+        e.preventDefault();
+        $(".catalog-item__content")
+          .eq(i)
+          .toggleClass("catalog-item__content_active");
+        $(".catalog-item__list")
+          .eq(i)
+          .toggleClass("catalog-item__list_active");
+      });
+    });
+  }
+
+  toggleSlide(".catalog-item__link");
+  toggleSlide(".catalog-item__back");
 });
